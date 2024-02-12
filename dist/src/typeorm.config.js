6ef6d122e874e34d357d4e63dcf5d3aacf924b5a
Mario Prieta Sánchez <145470491+mariops03@users.noreply.github.com>
2024-02-12 18:21:32 +0100

    Add User entity to typeorm.config.js and typeorm.config.ts

--- dist/src/typeorm.config.js
+++ dist/src/typeorm.config.js
@@ -6,10 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
 const dotenv_1 = __importDefault(require("dotenv"));
 const Product_1 = require("./entities/Product");
+const User_1 = require("./entities/User");
 dotenv_1.default.config();
 exports.default = new typeorm_1.DataSource({
     type: "postgres",
     url: process.env.CONNECTION_STRING,
-    entities: [Product_1.Product],
+    entities: [Product_1.Product, User_1.User],
     synchronize: true,
 });
